f4f5645cc04712b7c2a0e87582505a7faabf9568
gabrielrdw20 <gabrielrodewald@gmail.com>
2022-05-01 23:56:13 +0200

    Added viewsets to API

--- rest/profiles_api/urls.py
+++ rest/profiles_api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSets, basename='hello-viewset')
+
 urlpatterns = [
     path('hello-view/', views.Hello.as_view()),
+    path('', include(router.urls))
 ]
--- rest/profiles_api/views.py
+++ rest/profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response  # return dict or a list
 from rest_framework import status  # statuses, GET, POST, etc.
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class Hello(APIView):
@@ -40,8 +41,48 @@ class Hello(APIView):
     def patch(self, request, pk=None):
         """Handle a partial update of an object"""
         return Response({'method': 'PATCH'})
-    
 
     def delete(self, request, pk=None):
         """Deleting the whole object"""
-        return Response({'method' : 'DELETE'})
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message, just like GET request in APIView"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hellow message"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Welcome {name}!"
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Retrieve a specific object from database"""
+        return Response({"http_method": "GET"})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({"http_method": "PUT"})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({"http_method": "PATCH"})
+    
+    def destroy(self, request, pk=None):
+        """Handle deleting an object"""
+        return Response({"http_method": "DELETE"})
