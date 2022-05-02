d9588838382d58de9080c10cd20fd8c68ef73baa
gabrielrdw20 <gabrielrodewald@gmail.com>
2022-05-02 20:05:35 +0200

    Added filter, permissions and VieSet classes

--- rest/profiles_api/views.py
+++ rest/profiles_api/views.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response  # return dict or a list
 from rest_framework import status  # statuses, GET, POST, etc.
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+from rest_framework import filters
 
 
 class Hello(APIView):
@@ -82,7 +86,17 @@ class HelloViewSets(viewsets.ViewSet):
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
         return Response({"http_method": "PATCH"})
-    
+
     def destroy(self, request, pk=None):
         """Handle deleting an object"""
         return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSeriaizer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email', )
